54ce2cc5acb0f8fbaa581a5cd90545104fa6eb2c
Kyle S. Root <kroot27@gmail.com>
2018-11-08 18:17:47 -0700

    Primary graph created.  Still some problems with loading and resolution.

---
+++ number_3_graph_kroot.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(lubridate)
+library(sf)
+library(maps)
+
+homicides <- read_csv("data/homicide-data.csv")
+head(homicides, 10)
+
+homicides <- homicides %>% 
+  mutate(unsolved = disposition == "Closed without arrest" | disposition == "Open/No arrest",
+         reported_date = date(ymd(reported_date))) %>%
+  unite(city_name,
+        city,
+        state,
+        sep = ", ") %>% 
+  select(-uid,
+         -victim_last,
+         -victim_first,
+         -disposition) %>% 
+  filter(lon != "NA")
+
+us_states <- map("state", plot = FALSE, fill = TRUE) %>% 
+  st_as_sf()
+
+map_homicides <- st_as_sf(homicides, coords = c("lon", "lat")) %>% 
+  st_set_crs(4269)
+
+ggplot() +
+  geom_sf(data = us_states,
+          color = "white",
+          fill = "darkcyan",
+          alpha = 0.5) +
+  geom_sf(data = map_homicides, aes(color = victim_sex, shape = victim_race))
